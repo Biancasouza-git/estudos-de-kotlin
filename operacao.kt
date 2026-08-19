dc264bd14942bc2ea3f5d9b3dbd7605d0a7dfa4a
Bianca Souza <bixrtsouza@gmail.com>
2026-08-19 09:12:53 -0300

    Update operacao.kt

--- operacao.kt
+++ operacao.kt
@@ -1,1 +1,19 @@
+fun main(){
+  println("Escolha o primeiro numero:")
+  val n1=readln().toIntOrNull() ?: 0 
+  
+  println("Escolha o segundo numero:")
+  val n2=readln().toIntOrNull() ?: 0 
 
+  println("Escolha um operador: (+ - * / )")
+  val opcao=readln()
+
+  val result = when (opcao){
+    "+" -> n1+n2
+    "-" -> n1-n2
+    "*" -> n1*n2
+    "/" -> n1/n2
+    else -> "Opção invalida"
+  }
+  println(result)
+}
